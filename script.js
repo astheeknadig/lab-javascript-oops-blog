437b3d5de6d0e82421f88964f7276b323b2cf4cc
astheeknadig <64676602+astheeknadig@users.noreply.github.com>
2022-03-16 17:24:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 /* Fill your code*/
+class Blog{
+    constructor(title,details){
+        this.title = title;
+        this.details = details;
+    }
+
+    addTitle(){
+        var title_ele = document.createElement('h1');
+        title_ele.setAttribute("id","blog-title");
+        document.getElementById('card-text').appendChild(title_ele);
+        title_ele.innerHTML += this.title;
+    }
+
+    addDescription(){
+        var des_ele = document.createElement('p');
+        des_ele.setAttribute('id','blog-despriction');
+        document.getElementById('card-text').appendChild(des_ele);
+        des_ele.innerHTML += this.details;
+    }
+}
+
+document.getElementById('addBlog').onclick = () => {
+    document.getElementById("popupContact").style.display="block";
+}
+
+document.getElementById('close').onclick = () => {
+    document.getElementById("popupContact").style.display="none";
+}
+
+document.getElementById('post').onclick = () => {
+    document.getElementById("popupContact").style.display="none";
+    var t = document.getElementById('title').value;
+    var d = document.getElementById('detail').value;
+    var b = new Blog(t,d)
+    b.addTitle();
+    b.addDescription();
+    document.getElementById('detail').value = "";
+    document.getElementById('title').value = "";
+}
